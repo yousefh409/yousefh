9a6472884842746106222db34a7244e1142579bd
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2021-08-23 21:18:08 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,7 +97,7 @@ export default function Home() {
           About
         </div>
         <div className={indexStyles.aboutInfo}>
-          I am a Junior currently studying Electrical Engineering and Computer Science at the University of California, Berkeley. Previous to that, I attended De Anza College, and have since transferred over here. On the industry side, I have experience in Mobile and Web Development, but I am always eager to learn more about new areas! My research interests mainly lie in the fields of Robotics, Quantum Computing, and Digital Signal Processing, but I enjoy a wide array of CS and EE subfields. In my spare time, I enjoy playing video games (Rocket League and Valorant mainly at the moment), as well as playing Basketball, Soccer, and Tennis.
+          I am a Junior currently studying Electrical Engineering and Computer Science at the University of California, Berkeley. On the industry side, I have experience in Mobile and Web Development, but I am always eager to learn more about new areas! My research interests mainly lie in the fields of Robotics, Quantum Computing, and Digital Signal Processing, but I enjoy a wide array of CS and EE subfields. In my spare time, I enjoy playing video games (Rocket League and Valorant mainly at the moment), as well as playing Basketball, Soccer, and Tennis.
         </div>
       </div>
 
